5938c86ffda9770c1d0014f7aecfa2745d0e13be
Mehedi Hasan <mhasan.dev2021@gmail.com>
2026-02-26 12:11:49 +0600

    add new cors url

--- core/settings.py
+++ core/settings.py
@@ -184,6 +184,7 @@ TWILIO_VERIFY_SERVICE_SID = "VAxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://localhost:3001",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
