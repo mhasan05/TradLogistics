b23242bde536409b0582bedd0b4d0294a218960d
Mehedi Hasan <mhasan.dev2021@gmail.com>
2026-03-14 09:02:03 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -186,6 +186,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
+    "https://tradlogistics.com",
+    "https://www.tradlogistics.com",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
